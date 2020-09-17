5d10f01c1508915cd745b31a48a8e9e67c50d1db
Adarsh Hegde <adhegde001@gmail.com>
2020-09-17 23:18:44 +0530

    more stuff

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require("express")
 const cors = require("cors")
+const morgan = require("morgan")
+const helmet = require("helmet")
 
 const middlewares = require("./middlewares")
+
 const app = express()
+
+app.set('trust proxy', 1);
+app.use(morgan('dev'));
 app.use(cors())
+app.use(helmet());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
